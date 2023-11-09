a027e5891e302bad31be6f4d951a9c43f410b236
Banbie <drakwon21@gmail.com>
2023-11-09 13:04:15 +0900

    231109_selectcontrolstatement_If

---
+++ chaptor03_operators/src/sec01_selelctcontrolstatement/EX01_IfControlStatement/IfControlStatement.java
@@ -0,0 +1,76 @@
+package sec01_selelctcontrolstatement.EX01_IfControlStatement;
+
+public class IfControlStatement {
+
+	public static void main(String[] args) {
+		//if
+		int value1 = 5;
+		if(value1>3) {
+			System.out.println("실행1");
+		}
+		if(value1<5) {
+			System.out.println("실행2");
+		}
+		
+		boolean bool1 =true;
+		boolean bool2 =false;
+		if(bool1) {
+			System.out.println("실행3");
+		}
+		if(bool2) {
+			System.out.println("실행4");
+		}
+		
+		//if else
+		int value2 = 5;
+		if(value2>3) {
+			System.out.println("실행5");
+		} else {
+			System.out.println("실행6");
+		}
+		
+		//conditionalOperator
+		System.out.println((value2>3)?"실행5":"실행6");
+		System.out.println();
+		
+		//if else...if else
+		int value3 =85;
+		if(value3>=90) {
+			System.out.println("A학점");
+		} else if(value3>=80) {
+			System.out.println("B학점"); //실행 후 break
+		} else if(value3>=70) {
+			System.out.println("C학점");
+		} else {
+			System.out.println("F학점");
+		}
+		
+		if(value3>=70) {
+			System.out.println("C학점"); //실행 후 break
+		}
+		else if(value3>=80) {
+			System.out.println("B학점");
+		}
+		else if(value3>=90) {
+			System.out.println("A학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+		
+		if(value3>=70 && value3<80) {
+			System.out.println("C학점");
+		}
+		else if(value3>=80 && value3<90) {
+			System.out.println("B학점");
+		}
+		else if(value3>=90) {
+			System.out.println("A학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+
+	}
+
+}
