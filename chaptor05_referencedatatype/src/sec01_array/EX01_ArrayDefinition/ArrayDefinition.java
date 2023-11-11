27e18028e730d6db0fa19984c9284fd89b564e3a
Banbie <drakwon21@gmail.com>
2023-11-11 15:22:36 +0900

    20231111_referencedatatype_ArrayDefinition

---
+++ chaptor05_referencedatatype/src/sec01_array/EX01_ArrayDefinition/ArrayDefinition.java
@@ -0,0 +1,22 @@
+package sec01_array.EX01_ArrayDefinition;
+
+public class ArrayDefinition {
+	public static void main(String[] args) {
+		//array def1
+		int[] array1 = new int[3];
+		int[] array2;
+		array2 = new int [3];
+		
+		//array def2
+		int array3[] = new int[3];
+		int array4[];
+		array4 = new int[3];
+		
+		//ref type
+		boolean[] array5 = new boolean[3];
+		int[] array6 = new int[5];
+		double[] array7 = new double[3];
+		String[] array8 = new String[9];
+	}
+
+}
