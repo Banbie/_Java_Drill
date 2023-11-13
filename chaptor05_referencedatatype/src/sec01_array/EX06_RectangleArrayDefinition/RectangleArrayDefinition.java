549d847153bc3d2d259ed741e70e1e1d6d603286
Banbie <drakwon21@gmail.com>
2023-11-13 10:08:44 +0900

    231113_referencedatatype_RectangleArrayDefinition

---
+++ chaptor05_referencedatatype/src/sec01_array/EX06_RectangleArrayDefinition/RectangleArrayDefinition.java
@@ -0,0 +1,26 @@
+package sec01_array.EX06_RectangleArrayDefinition;
+
+public class RectangleArrayDefinition {
+	public static void main(String[] args) {
+		//Array Def1
+		int[][] array1 = new int[3][4];
+		int [][] array2;
+		array2 = new int[3][4];
+		
+		//Array Def1
+		int array3[][] = new int [3][4];
+		int array4[][];
+		array4 = new int [3][4];
+		
+		//Array Def1
+		int [] array5[] = new int [3][4];
+		int[] array6[];
+		array6 = new int [3][4];
+		
+		//Array type
+		boolean[][] array7 = new boolean[3][4];
+		int [][] array8 = new int [2][4];
+		double[][] array9 = new double [3][5];
+		String[][] array10 = new String [2][6]; //Reference Type
+	}
+}
