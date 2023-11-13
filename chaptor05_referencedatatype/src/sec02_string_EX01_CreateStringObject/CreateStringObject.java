f0855b3345c1a0474ffb1acd96ca845ade578c28
Banbie <drakwon21@gmail.com>
2023-11-13 19:48:45 +0900

    231113_refdatatype_CreateStringObject

---
+++ chaptor05_referencedatatype/src/sec02_string_EX01_CreateStringObject/CreateStringObject.java
@@ -0,0 +1,13 @@
+package sec02_string_EX01_CreateStringObject;
+
+public class CreateStringObject {
+	public static void main(String[] args) {
+		//1
+		String str1 = new String("Hi");
+		System.out.println(str1);
+		
+		//2
+		String str2 = "Hello";
+		System.out.println(str2);
+	}
+}
