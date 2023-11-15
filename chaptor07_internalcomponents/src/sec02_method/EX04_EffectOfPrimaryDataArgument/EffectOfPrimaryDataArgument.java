b768f08abd5e7533bc153b55d4f4efa60ab6f441
Banbie <drakwon21@gmail.com>
2023-11-15 21:19:05 +0900

    231115_comp_EffectOfReferenceDataArgument

--- chaptor07_internalcomponents/src/sec02_method/EX04_EffectOfPrimaryDataArgument/EffectOfPrimaryDataArgument.java
+++ chaptor07_internalcomponents/src/sec02_method/EX04_EffectOfPrimaryDataArgument/EffectOfPrimaryDataArgument.java
@@ -2,7 +2,7 @@ package sec02_method.EX04_EffectOfPrimaryDataArgument;
 
 public class EffectOfPrimaryDataArgument {
 	public static void main(String[] args) {
-		int a = 3;
+		int a = 3; //stack
 		int result1 = twice(3);
 		System.out.println(result1);
 		int result2 = twice(a);
@@ -11,7 +11,7 @@ public class EffectOfPrimaryDataArgument {
 	}
 	
 	public static int twice(int a) {
-		a = a*2;
+		a = a*2; //stack
 		return a;
 	}
 }
