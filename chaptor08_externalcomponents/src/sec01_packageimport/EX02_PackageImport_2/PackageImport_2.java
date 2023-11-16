55ce5b3cb70126178fb232215d32d026dde2ed05
Banbie <drakwon21@gmail.com>
2023-11-16 15:19:24 +0900

    231116_comp_PackageImport

---
+++ chaptor08_externalcomponents/src/sec01_packageimport/EX02_PackageImport_2/PackageImport_2.java
@@ -0,0 +1,13 @@
+package sec01_packageimport.EX02_PackageImport_2;
+
+import sec01_packageimport.common.A;
+
+public class PackageImport_2 {
+	public static void main(String[] args) {
+		A a = new A();
+		
+		System.out.println(a.m);
+		System.out.println(a.n);
+		a.print();
+	}
+}
