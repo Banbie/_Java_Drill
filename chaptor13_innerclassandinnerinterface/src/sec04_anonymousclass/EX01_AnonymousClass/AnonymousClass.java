3e45db04793f2c32f5a3afe344fb7a74740d34f5
Banbie <drakwon21@gmail.com>
2023-11-23 00:23:48 +0900

    231123_anonymousclass_comparetoinstanceinnerclass

--- chaptor13_innerclassandinnerinterface/src/sec04_anonymousclass/EX01_AnonymousClass/AnonymousClass.java
+++ chaptor13_innerclassandinnerinterface/src/sec04_anonymousclass/EX01_AnonymousClass/AnonymousClass.java
@@ -1,22 +1,32 @@
 package sec04_anonymousclass.EX01_AnonymousClass;
 
 class A {
-	C c1 = new B();
-	
-	C c2 = new C() {
+	B b = new B();
+	C c2 = (C) b;
+	C c1 = new C() {
 		public void bcd() {
 			System.out.println("Anonymous InnerClass");
+			addedMethod(); //only call inner method
+		}
+		public void addedMethod() {
+			System.out.println("Anonymous InnerClass addedMethod");
 		}
 	};
 	
 	void abc() {
+		b.bcd();
+		b.addedMethod();
+		System.out.println();
 		c1.bcd();
-		
+		System.out.println();
 		c2.bcd();
 	}
 	class B implements C {
 		public void bcd() {
 			System.out.println("Instance InnerClass");
+		}
+		public void addedMethod() {
+			System.out.println("Instance InnerClass addedMethod");
 		}
 	}
 }
