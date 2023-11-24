22fd37681b262558f8f0850d2c55d21798edb095
Banbie <drakwon21@gmail.com>
2023-11-24 23:25:35 +0900

    231124_exception_TryCatchFinally

---
+++ chaptor14_exceptionhandling/src/sec02_exceptionhandlingsyntax/EX01_TryCatchFinally/TryCatchFinally.java
@@ -0,0 +1,24 @@
+package sec02_exceptionhandlingsyntax.EX01_TryCatchFinally;
+
+public class TryCatchFinally {
+	public static void main(String[] args) {
+		try {
+			System.out.println(3/0);
+			System.out.println("Tried"); //run even exception
+		} 
+		catch (ArithmeticException e) {
+			System.out.println("can't divide zero");
+		}
+		
+		try {
+			System.out.println(3/0);
+			System.out.println("Tried");
+		}
+		catch (ArithmeticException e) {
+			System.out.println("can't divide zero");
+		}
+		finally {
+			System.out.println("program quit");
+		}
+	}
+}
