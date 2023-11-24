7aee2bd9b7c1d2e6fdae4c0749bf5f80e21dcfdf
Banbie <drakwon21@gmail.com>
2023-11-24 20:16:46 +0900

    231124_exception_generalException

---
+++ chaptor14_exceptionhandling/src/sec01_typeofexception/EX01_CheckedException/CheckedException.java
@@ -0,0 +1,34 @@
+package sec01_typeofexception.EX01_CheckedException;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+class A implements Cloneable {
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
+
+public class CheckedException {
+	public static void main(String[] args) {
+		//Exception
+		/*
+		// 1. InterruptedException
+		Thread.sleep(1000);
+		
+		// 2. ClassNotFoundException
+		Class cls = Class.forName("java.lang.Object");
+		
+		// 3. IOException
+		InputStreamReader in = new InputStreamReader(System.in);
+		in.read();
+		
+		// 4. FileNotFoundException
+		FileInputStream fis = new FileInputStream("text.txt");
+		
+		// 5. CloneNotSupportedException
+		A a1 = new A();
+		A a2 =(A)a1.clone();
+		*/
+	}
+}
