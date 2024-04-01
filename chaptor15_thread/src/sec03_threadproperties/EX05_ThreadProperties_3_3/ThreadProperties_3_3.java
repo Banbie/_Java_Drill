57a98b075857d0fc1556be178700d0128ba5e869
Banbie <drakwon21@gmail.com>
2024-04-01 19:12:04 +0900

    240401_ThreadProperties_3_3

---
+++ chaptor15_thread/src/sec03_threadproperties/EX05_ThreadProperties_3_3/ThreadProperties_3_3.java
@@ -0,0 +1,35 @@
+package sec03_threadproperties.EX05_ThreadProperties_3_3;
+
+
+class MyThread extends Thread {
+	@Override
+	public void run() {
+		System.out.println(getName() + " : " + (isDaemon() ? "데몬 쓰레드":"일반 쓰레드"));
+		
+		for(int i =0; i<6; i++) {
+			System.out.println(getName() + " : " + i +"초");
+			try {Thread.sleep(1000);} catch(InterruptedException e) {}
+		}
+	}
+}
+
+public class ThreadProperties_3_3 {
+	public static void main(String[] args) {
+		
+		//일반 쓰레드
+		Thread thread1 = new MyThread();
+		thread1.setDaemon(false);
+		thread1.setName("thread1");
+		thread1.start();
+		
+		//데몬 쓰레드
+		Thread thread2 = new MyThread();
+		thread2.setDaemon(true);
+		thread2.setName("thread2");
+		thread2.start();
+		
+		//3.5초 후 main 쓰레드 종료
+		try {Thread.sleep(3500);} catch (InterruptedException e) {}
+		System.out.println("main Thread 종료");
+	}
+}
