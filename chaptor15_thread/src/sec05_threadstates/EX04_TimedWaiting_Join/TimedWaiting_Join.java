7383012670a2e0a9be740c2c02511f01674e1e6f
Banbie <drakwon21@gmail.com>
2024-05-14 13:25:49 +0900

    240514_TimeWaiting_Join add

--- chaptor15_thread/src/sec05_threadstates/EX04_TimedWaiting_Join/TimedWaiting_Join.java
+++ chaptor15_thread/src/sec05_threadstates/EX04_TimedWaiting_Join/TimedWaiting_Join.java
@@ -3,8 +3,8 @@ package sec05_threadstates.EX04_TimedWaiting_Join;
 class MyThread1 extends Thread {
 	@Override
 	public void run() {
-		for(int i =0; i < 100; i++) {
-			System.out.println(i);
+		for(int i =0; i < 1000; i++) {
+			System.out.println(1000-i+"myThread1");
 		} // 시간지연
 	}
 }
@@ -22,7 +22,9 @@ class MyThread2 extends Thread {
 			myThread1.join(3000);
 		} catch (InterruptedException e) {
 			System.out.println(" -- join(...) 진행중 interrupt() 발생");
-			for(int i =0; i < 100000000; i++) {} // 시간지연
+			for(int i =0; i < 1000; i++) {
+				System.out.println(i+"myThread2");
+			} // 시간지연
 		}
 	}
 }
@@ -41,8 +43,9 @@ public class TimedWaiting_Join {
 		
 		// TIME_WAITING
 		myThread2.interrupt();
-		try {Thread.sleep(100);} catch (InterruptedException e) {}; // interrupt 준비시간
+		try {Thread.sleep(25);} catch (InterruptedException e) {}; // interrupt 준비시간 하드웨어에 따라 다름
 		System.out.println("MyThread1 State = " + myThread1.getState());
 		System.out.println("MyThread2 State = " + myThread2.getState());
+		try {Thread.sleep(3000);} catch (InterruptedException e) {}; // main thread 유지
 	}
 }
