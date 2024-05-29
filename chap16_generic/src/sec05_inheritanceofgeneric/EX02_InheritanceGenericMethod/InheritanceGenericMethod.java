12cdd6fa46808f19f111ab81c8b87ce9c157b01f
Banbie <drakwon21@gmail.com>
2024-05-29 23:30:10 +0900

    240529_inheritanceGenericMethod

---
+++ chap16_generic/src/sec05_inheritanceofgeneric/EX02_InheritanceGenericMethod/InheritanceGenericMethod.java
@@ -0,0 +1,22 @@
+package sec05_inheritanceofgeneric.EX02_InheritanceGenericMethod;
+
+class Parent {
+	<T extends Number> void print(T t) {
+		System.out.println(t);
+	}
+}
+
+class Child extends Parent {}
+
+public class InheritanceGenericMethod {
+	public static void main(String[] args) {
+		
+		Parent p = new Parent();
+		p.<Integer>print(10);
+		p.print(10);
+		
+		Child c = new Child();
+		c.<Double>print(5.8);
+		c.print(5.8);
+	}
+}
