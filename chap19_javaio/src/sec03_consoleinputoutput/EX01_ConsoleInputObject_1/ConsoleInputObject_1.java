d3582ee88ffa6d5d0fc7a537ca924a0955e14c43
Banbie <drakwon21@gmail.com>
2024-07-05 20:42:37 +0900

    240705_consoleinputobject_1

---
+++ chap19_javaio/src/sec03_consoleinputoutput/EX01_ConsoleInputObject_1/ConsoleInputObject_1.java
@@ -0,0 +1,15 @@
+package sec03_consoleinputoutput.EX01_ConsoleInputObject_1;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ConsoleInputObject_1 {
+	public static void main(String[] args) throws IOException {
+		// InputStream 객체 생성
+		InputStream is = System.in;
+		int data;
+		while((data = is.read()) != '\r')
+			System.out.println("읽은 데이터: " + (char)data + "   남은 바이트 수: " + is.available());
+		System.out.println(data); // \r
+		System.out.println(is.read()); // 버퍼에 남아있는 \n 해소
+	}
+}
