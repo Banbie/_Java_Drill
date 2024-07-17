ee98d2913e8cfa7f839b7134327700376310d3b3
Banbie <drakwon21@gmail.com>
2024-07-17 22:23:42 +0900

    240717_inputstreamreader_2

---
+++ chap19_javaio/src/sec05_readerwriter/EX04_InputStreamReader_2/InputStreamReader_2.java
@@ -0,0 +1,24 @@
+package sec05_readerwriter.EX04_InputStreamReader_2;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReader_2 {
+	public static void main(String[] args) {
+		// 콘솔로 입력(UTF-8)
+		try {
+			InputStreamReader isr = new InputStreamReader(System.in, "UTF-8");
+			int data;
+			while((data = isr.read()) != '\r')
+				System.out.print((char)data);
+			System.out.println("\n" + isr.getEncoding());
+		} catch (IOException e) {}
+		// 콘솔로 입력(MS949)
+		try {
+			InputStreamReader isr = new InputStreamReader(System.in, "MS949");
+			int data;
+			while((data = isr.read()) != '\r')
+				System.out.print((char)data);
+			System.out.println("\n" + isr.getEncoding());
+		} catch(IOException e) {}
+	}
+}
